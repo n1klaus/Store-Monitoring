a57a9dfad79e3636365c9bc2d08d879ab6b2318b
Nick Nyanjui <nicknyanjui@gmail.com>
2023-09-14 17:31:53 +0300

    fix: update configuration settings for celery and app exports

---
+++ config/tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Celery task handler module"""
+from functools import lru_cache
+
+from celery import Celery
+
+from config.settings import get_settings
+
+settings = get_settings()
+
+
+@lru_cache()
+def get_celery():
+    """Returns celery instance"""
+    celery: Celery = Celery("store_monitoring", include=["jobs.job_engine"])
+    celery.config_from_object(settings, force=True, namespace="CELERY")
+    return celery
