bdbf21220d003be98715288806855edd16bd7695
Nick Nyanjui <nicknyanjui@gmail.com>
2024-01-26 13:15:16 +0300

    fix: update server script to allow celery to fully start

--- server.sh
+++ server.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+## Close any existing celery worker nodes
+pkill -f "celery worker" && \
+
 ## Start celery worker
-celery -A config.celery worker --loglevel=info & disown;
+celery -A config.celery worker --loglevel=info & disown && \
+
+# Wait for celery worker to start
+sleep 5s && \
 
 ## Start uvicorn server
-python3 main.py;
+python3 main.py && \
 
 exit 0;
